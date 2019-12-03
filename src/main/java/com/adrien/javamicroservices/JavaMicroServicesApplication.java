316d4d52bc03ab417c11bab772f396ae61f846d2
adrienvaucard <adrien.vaucard@gmail.com>
2019-12-03 09:34:04 +0100

    feat(back): Initial Commit

---
+++ src/main/java/com/adrien/javamicroservices/JavaMicroServicesApplication.java
@@ -0,0 +1,13 @@
+package com.adrien.javamicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMicroServicesApplication.class, args);
+	}
+
+}
